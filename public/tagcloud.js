1ab56a600a5976eb768a16986fa340e26c0c2f79
Sergey Panov <gostroy@gmail.com>
2016-09-10 23:50:50 +0300

    Define your schemas

--- public/tagcloud.js
+++ public/tagcloud.js
@@ -5,6 +5,8 @@ require('plugins/gostroy_k_46_p_exm_tagcloud/tagcloud.css');
 function TagcloudProvider(Private) {
   // Load TemplateVisType
   var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
+  // Include the Schemas class, which will be used to define schemas
+  var Schemas = Private(require('ui/Vis/Schemas'));
 
   // Return a new instance describing this visualization
   return new TemplateVisType({
@@ -12,7 +14,25 @@ function TagcloudProvider(Private) {
     title: 'Tagcloud', // the name shown in the visualize list
     icon: 'fa-cloud', // the class of the font awesome icon for this
     description: 'Tagcloud visualization', // description shown to the user
-    template: require('plugins/gostroy_k_46_p_exm_tagcloud/tagcloud.html') // Load the template of the visualization
+    template: require('plugins/gostroy_k_46_p_exm_tagcloud/tagcloud.html'), // Load the template of the visualization
+    schemas: new Schemas([
+      {
+        group: 'metrics',
+        name: 'tagsize',
+        title: 'Tagsize',
+        min: 1,
+        max: 1,
+        aggFilter: ['count', 'avg', 'sum', 'min', 'max', 'cardinality', 'std_dev']
+      },
+      {
+        group: 'buckets',
+        name: 'tags',
+        title: 'Tags',
+        min: 1,
+        max: 1,
+        aggFilter: '!geohash_grid'
+      }
+    ])
   });
 }
 
