8f30bc026dd0bad49af280e3cf541614acbf3c09
Sergey Panov <gostroy@gmail.com>
2016-09-10 23:07:23 +0300

    Writing Kibana 4 Plugins – Visualizations using Data : Define and register you visualization   https://www.timroes.de/2015/12/06/writing-kibana-4-plugins-visualizations-using-data/

---
+++ public/tagcloud.js
@@ -0,0 +1,21 @@
+// Include our custom CSS (LESS also works)
+require('plugins/gostroy_k_46_p_exm_timroes/tagcloud.css');
+
+// The provider function must return the visualization
+function TagcloudProvider(Private) {
+  // Load TemplateVisType
+  var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
+
+  // Return a new instance describing this visualization
+  return new TemplateVisType({
+    name: 'gostroyTagcloud', // the internal id of the visualization
+    title: 'Tagcloud', // the name shown in the visualize list
+    icon: 'fa-cloud', // the class of the font awesome icon for this
+    description: 'Tagcloud visualization', // description shown to the user
+    template: require('plugins/gostroy_k_46_p_exm_tagcloud/tagcloud.html') // Load the template of the visualization
+  });
+}
+
+// Register the above provider to the visualization registry
+require('ui/registry/vis_types').register(ClockProvider);
+
